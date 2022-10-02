dd4c402319be51cde28d1b11f8b798241dee4945
Marc Kramer <marc.kramer@polyfit42.com>
2022-10-02 22:03:20 +0200

    Add resources

--- create_thumbnails.sh
+++ create_thumbnails.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 cd split_0
 
 for f in *.obj; do
-    render_obj_file "$f" --scale 0.2 --blend_mode continuous --no_shadows --output /tmp/out.png
-    convert /tmp/out.png -trim -transparent 'rgb(255,0,255)' "${f%%.obj}_thumb.png"
+    render_obj_file "$f" --scale 0.3 --blend_mode continuous --no_shadows --look_at_aabb --output /tmp/out.png
+    convert /tmp/out.png -trim -transparent 'rgb(255,0,255)' -resize '50%' "${f%%.obj}_thumb.png"
     rm /tmp/out.png
 done;
